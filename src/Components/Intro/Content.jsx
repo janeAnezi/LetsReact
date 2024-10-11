362e8e8bf3512c3f3aa589dd228b48411822b010
Anezijane <ahnnieanezi@gmail.com>
2024-10-11 23:34:55 +0100

    function with argument

--- src/Components/Intro/Content.jsx
+++ src/Components/Intro/Content.jsx
@@ -10,10 +10,15 @@ export default function Content() {
     let handleClick = () => {
         alert(' button clicked!');
     }
+    let handleClick2 = (name) => {
+        alert(` button clicked!  ${name}`);
+    }
   return (
     <div className='content'>
       <h3>Let's Shop {handleNameChange()}</h3>
       <button style={{color: "brown"}} onClick={handleClick}>Click Me</button>
+
+      <p><button style={{color: "gray"}} onClick={() => {handleClick2('Anezi')}}>Click Again</button></p>
     </div>
   )
 }
