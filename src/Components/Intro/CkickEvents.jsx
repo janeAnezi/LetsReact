8ed4eb64335220a56117295533cefd8c27c08885
Anezijane <ahnnieanezi@gmail.com>
2024-10-11 23:42:23 +0100

    add click events

---
+++ src/Components/Intro/CkickEvents.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+export default function ClickEvents() {
+    let handleNameChange = () => {
+        let userNames = ['Blessing', 'James', 'Berry', 'Andy'];
+        let num = Math.floor((Math.random) * 4);
+        return userNames[num]
+    }
+
+    let handleClick = () => {
+        alert(' button clicked!');
+    }
+    let handleClick2 = (name) => {
+        alert(` button clicked! ${name}`);
+    }
+  return (
+    <div className='content'>
+      <h3 onDoubleClick={handleClick}>Let's Shop {handleNameChange()}</h3>
+      <button style={{color: "brown"}} onClick={handleClick}>Click Me</button>
+
+      <p><button style={{color: "gray"}} onClick={() => {handleClick2('Anezi')}}>Click Again</button></p>
+    </div>
+  )
+}
--- src/Components/Intro/Content.jsx
+++ src/Components/Intro/Content.jsx
@@ -1,24 +1,10 @@
 import React from 'react'
 
 export default function Content() {
-    let handleNameChange = () => {
-        let userNames = ['Blessing', 'James', 'Berry', 'Andy'];
-        let num = Math.floor((Math.random) * 4);
-        return userNames[num]
-    }
-
-    let handleClick = () => {
-        alert(' button clicked!');
-    }
-    let handleClick2 = (name) => {
-        alert(` button clicked!  ${name}`);
-    }
+    
   return (
     <div className='content'>
-      <h3>Let's Shop {handleNameChange()}</h3>
-      <button style={{color: "brown"}} onClick={handleClick}>Click Me</button>
-
-      <p><button style={{color: "gray"}} onClick={() => {handleClick2('Anezi')}}>Click Again</button></p>
+     
     </div>
   )
 }
