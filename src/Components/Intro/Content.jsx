6e90dbfeeaa51cdf93b613936620b1af49eaeef9
Anezijane <ahnnieanezi@gmail.com>
2024-10-11 23:52:15 +0100

    React Hook (useState)

--- src/Components/Intro/Content.jsx
+++ src/Components/Intro/Content.jsx
@@ -1,9 +1,18 @@
 import {useState} from 'react'
 
 export default function Content() {
+    const [name, setName] = useState('Jude')
+
+    let ChangeName = () => {
+        let names = ['Ngozi', 'Shemaiah', 'Ovadia', 'Jane'];
+        let nameIndex = Math.floor(Math.random() * 4)
+        setName(names[nameIndex])
+    }
     
   return (
     <div className='content'>
+        <h3>Hello {name}</h3>
+        <button style={{color: 'green'}} onClick={ChangeName}>Change name</button>
      
     </div>
   )
