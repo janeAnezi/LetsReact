49dc685c6c91417acddbd22f42b72f06f9b2dfb6
Anezijane <ahnnieanezi@gmail.com>
2024-10-12 22:51:25 +0100

    Add list and keys

--- src/Components/Intro/ListAndKeys.jsx
+++ src/Components/Intro/ListAndKeys.jsx
@@ -1,6 +1,11 @@
-import React from 'react'
+import {useState} from 'react'
 
 export default function ListAndKeys() {
+    const [item, setItem] = useState([
+        { id: 1, name: 'Item 1', check: false },
+        { id: 2, name: 'Item 2', check: false},
+        { id: 3, name: 'Item 3', check: false },
+    ])
   return (
     <div>
       
