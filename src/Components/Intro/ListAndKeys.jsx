dbfe663cd24d6ce1aabcdd0750219be4b01be018
Anezijane <ahnnieanezi@gmail.com>
2024-10-12 23:57:19 +0100

    React styles

--- src/Components/Intro/ListAndKeys.jsx
+++ src/Components/Intro/ListAndKeys.jsx
@@ -6,14 +6,22 @@ export default function ListAndKeys() {
         { id: 1, name: 'Item 1', checked: false },
         { id: 2, name: 'Item 2', checked: false},
         { id: 3, name: 'Item 3', checked: false },
-    ])
+    ]);
+
+    let HandleCheck = (id) => {
+        console.log(`key: ${id}`);
+
+    }
   return (
     <div className='content'>
         <ul>
             {items.map((item) => (
                 <li className='item' key={item.id}>
                     <div>
-                        <input type="checkbox" checked={item.checked}
+                        <input 
+                            onChange={()=>{HandleCheck(item.id)}}
+                            type="checkbox" 
+                            checked={item.checked}
                         />
                         <label>{item.name}</label>
                     </div>
