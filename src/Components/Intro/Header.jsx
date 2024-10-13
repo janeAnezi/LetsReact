a7b13efffa84b78e3ea63389208194fff822e1c0
Anezijane <ahnnieanezi@gmail.com>
2024-10-13 15:28:13 +0100

    add icon to add item and update styles

--- src/Components/Intro/Header.jsx
+++ src/Components/Intro/Header.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import { IoMdAdd } from "react-icons/io";
 
 export default function Header() {
   return (
     <div className='header'>
       <h1>Grocery List</h1>
+      <div className="add">
+        <IoMdAdd className="plus"/> 
+        <sup>Add Item</sup>
+      </div>
     </div>
   )
 }
--- src/Components/Intro/ListAndKeys.jsx
+++ src/Components/Intro/ListAndKeys.jsx
@@ -1,11 +1,13 @@
 import {useState} from 'react'
 import { IoTrash } from "react-icons/io5";
 
+
 export default function ListAndKeys() {
     const [items, setItem] = useState([
         { id: 1, name: 'Item 1', checked: false },
         { id: 2, name: 'Item 2', checked: false},
         { id: 3, name: 'Item 3', checked: false },
+        { id: 4, name: 'Item 4', checked: false },
     ]);
 
     let HandleCheck = (id) => {
@@ -15,27 +17,39 @@ export default function ListAndKeys() {
 
        localStorage.setItem('ShopingList', JSON.stringify(listItems));
     }
+
+    let HandleDelete = (id) => {
+       let listItems = items.filter((item) => item.id !== id);
+       setItem(listItems);
+       localStorage.setItem('Shopinglist', JSON.stringify(listItems))
+    }
+
   return (
     <div className='content'>
-        <ul>
-            {items.map((item) => (
-                <li className='item' key={item.id}>
-                    <div>
-                        <input 
-                            onChange={()=>{HandleCheck(item.id)}}
-                            type="checkbox" 
-                            checked={item.checked}
-                        />
-                        <label 
-                            style={(item.checked) ? {textDecoration: 'line-through'}: null}
-                            onDoubleClick={()=>{HandleCheck(item.id)}}>
-                            {item.name}
-                        </label>
-                    </div>
-                    <IoTrash className='svg' role='button' tabIndex='0'/>
-                </li>
-            ))}
-        </ul>
+        {(items > 0) ? (
+            <ul> 
+                {items.map((item) => (
+                    <li className='item' key={item.id}>
+                        <div>
+                            <input 
+                                onChange={()=>{HandleCheck(item.id)}}
+                                type="checkbox" 
+                                checked={item.checked}
+                            />
+                            <label 
+                                style={(item.checked) ? {textDecoration: 'line-through'}: null}
+                                onDoubleClick={()=>{HandleCheck(item.id)}}>
+                                {item.name}
+                            </label>
+                        </div>
+                        <IoTrash
+                        onClick={() => {HandleDelete(item.id)}}
+                        className='svg' role='button' tabIndex='0'/>
+                    </li>
+                ))}
+            
+            </ul> )
+        :<p>No Items on the list</p>}
     </div>
   )
 }
