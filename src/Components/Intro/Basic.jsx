3008a7ffb815818a88b0a8482e173bb361a7ec6b
Anezijane <ahnnieanezi@gmail.com>
2024-10-18 21:35:53 +0100

    set state to load from local storage

--- src/Components/Intro/Basic.jsx
+++ src/Components/Intro/Basic.jsx
@@ -7,12 +7,7 @@ import './App.css'
 
 
 export default function Basic() {
-  const [items, setItem] = useState([
-    { id: 1, name: 'Item 1', checked: false },
-    { id: 2, name: 'Item 2', checked: false},
-    { id: 3, name: 'Item 3', checked: false },
-    { id: 4, name: 'Item 4', checked: false },
-]);
+  const [items, setItem] = useState(JSON.parse(localStorage.getItem('ShopingList')));
 
 
 const setAndSaveItems = (newItems) => {
