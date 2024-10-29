a5b2eeda64835af685c8dc58e3ab4ce7fb8492f0
Anezijane <ahnnieanezi@gmail.com>
2024-10-29 10:16:33 +0100

    useState hook

--- src/Components/LetsReact/UseStateComp.jsx
+++ src/Components/LetsReact/UseStateComp.jsx
@@ -2,17 +2,29 @@ import { useState } from "react"
 
 export default function UseStateComp() {
 
-  const [color, setColor] = useState('Red')
+//   const [color, setColor] = useState('Red')
 
-  // let color = "Red"
- let changeFavColor = () => {
-  setColor("Blue")
- }
+//   // let color = "Red"
+//  let changeFavColor = () => {
+//   setColor("Blue")
+//  }
+
+
+const [brand, setBrand] = useState('Ferrari')
+const [model, setModel] = useState('Roma')
+const [year, setyear] = useState('2023')
+const [color, setColor] = useState('Red')
 
   return (
     <div>
-      <h1>My favourite color is {color}</h1>
-      <button onClick={changeFavColor}>Change</button>
+      {/* <h1>My favourite color is {color}</h1>
+      <button onClick={changeFavColor}>Change</button> */}
+
+
+      {/* Multiple State Variable */}
+
+      <h1>My {brand}</h1>
+      <h3>It is a {color} {model} from {year}</h3>
     </div>
   )
 }
