07aa67e811ef0207724cfe69ce455d2232d634a7
Anezijane <ahnnieanezi@gmail.com>
2024-11-03 22:53:43 +0100

    add useMemo Hook

--- src/Components/LetsReact/UseMemoHook.jsx
+++ src/Components/LetsReact/UseMemoHook.jsx
@@ -4,8 +4,9 @@ export default function UseMemoHook() {
     const [number, setNumber] = useState(0)
     const [counter, setCounter] = useState(0)
 
-    function cubeNum() {
+    function cubeNum(num) {
         console.log("calculation done!");
+        return Math.pow(num, 3)
     }
   return (
     <div>
